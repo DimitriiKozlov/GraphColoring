69ab84d59d14441cbd73aa3ff82bea76d6dc29a4
Dimitrij Kozlov <ikzdimitrij@gmail.com>
2016-02-18 02:43:36 +0200

    Added received data method

--- AILab1/Graph.cs
+++ AILab1/Graph.cs
@@ -1,7 +1,38 @@
-﻿namespace AILab1
+﻿using System;
+
+namespace AILab1
 {
     class Graph
     {
+        private bool[,] _matrix;
+        private int _nVertex;
+        private int _nEdge;
+        //public int[] VertexColor;
 
+
+        public Graph(int nV, int nE)
+        {
+            _nEdge = nE;
+            _nVertex = nV;
+            _matrix = new bool[_nVertex, _nVertex];
+            //VertexColor = new int[_nVertex];
+        }
+
+        public void setData(string source)
+        {
+            var words = source.Split();
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i] == "e")
+                    continue;
+                var n = Convert.ToInt32(words[i]);
+                var m = Convert.ToInt32(words[i + 1]);
+                _matrix[n, m] = _matrix[m, n] = true;
+                i++;
+            }
+        }
+
+        
     }
 }
